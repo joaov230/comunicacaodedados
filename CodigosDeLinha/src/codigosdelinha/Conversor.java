1b53c5e6a18a698c036bee24a1003b0e606a31e4
João Vítor F. B <joaob20@hotmail.com>
2019-04-26 08:52:39 -0300

    4 codificações adicionadas

--- CodigosDeLinha/src/codigosdelinha/Conversor.java
+++ CodigosDeLinha/src/codigosdelinha/Conversor.java
@@ -27,31 +27,117 @@ public class Conversor {
     // e transformada para forma de sinal
     ////////////////////////////
     
+    // NRZ-L
     // 0 é sinal alto, 1 é sinal baixo
     public void nrzlCodificado (String str) {
+        sinal.clear();
         char c;
         for (int i = 0; i < str.length(); i++) {
             c = str.charAt(i);
-            if (c == 1) {
+            if (c == '1') {
                 sinal.add(BAIXO);
                 sinal.add(BAIXO);
-            } else if (c == 0) {
+            } else if (c == '0') {
                 sinal.add(ALTO);
                 sinal.add(ALTO);
             }
         }
     }
     
+    // NRZ-I
+    // 1 inverte o sinal, 0 mantém o sinal anterior
+    public void nrziCodificado (String str) {
+        sinal.clear();
+        char c;
+        for (int i = 0; i < str.length(); i++) {
+            c = str.charAt(i);
+            if (i == 0) { // Se for o primeiro bit, sinal começa em alto sempre
+                sinal.add(ALTO);
+                sinal.add(ALTO);
+            }
+            if (c == '1') {
+                Integer aux = inverteUltimoSinal();
+                sinal.add(aux);
+                sinal.add(aux);
+            } else if (c == '0') {
+                Integer aux = getUltimoSinal();
+                sinal.add(aux);
+                sinal.add(aux);
+            }
+        }
+    }
+    
+    // Bipolar-AMI
+    // 0 é sinal médio, 1 é alto ou baixo (inverso do último bit 1)
+    public void bamiCodificado (String str) {
+        sinal.clear();
+        char c;
+        boolean alto = true;
+        for (int i = 0; i < str.length(); i++) {
+            c = str.charAt(i);
+            if (c == '1') {
+                if (alto == true) { // Se o sinal anterior foi alto, o próximo será baixo
+                    sinal.add(BAIXO);
+                    sinal.add(BAIXO);
+                    alto = false;
+                } else { // Se o sinal anterior foi baixo, o próximo será alto
+                    sinal.add(ALTO);
+                    sinal.add(ALTO);   
+                }
+            } else if (c == '0') {
+                sinal.add(MEDIO);
+                sinal.add(MEDIO);
+            }
+        }
+    }
+    
+    // Bipolar Pseudoternário
+    // 0 é sinal médio, 1 é alto ou baixo (inverso do último bit 1)
+    public void pseudoternarioCodificado (String str) {
+        sinal.clear();
+        char c;
+        boolean alto = true;
+        for (int i = 0; i < str.length(); i++) {
+            c = str.charAt(i);
+            if (c == '0') {
+                if (alto == true) { // Se o sinal anterior foi alto, o próximo será baixo
+                    sinal.add(BAIXO);
+                    sinal.add(BAIXO);
+                    alto = false;
+                } else { // Se o sinal anterior foi baixo, o próximo será alto
+                    sinal.add(ALTO);
+                    sinal.add(ALTO);   
+                }
+            } else if (c == '1') {
+                sinal.add(MEDIO);
+                sinal.add(MEDIO);
+            }
+        }
+    }
+    
+    
+    
+    
     
     // Se o último sinal for alto, o próximo será baixo e vice versa
     // Útil para codificações como a Manchester, que o mesmo bit assume 2 valores na ArrayList sinal
-    private Integer inverte (Integer ultimoSinal) {
-        if (ultimoSinal == ALTO) { // Se o último sinal for alto, o próximo será baixo
+    private Integer inverteUltimoSinal () {
+        return inverte(getUltimoSinal());
+    }
+    
+    // Retorna o sinal invertido ao sinal enviado
+    private Integer inverte (Integer temp) {
+        if (temp == ALTO) { // Se o último sinal for alto, o próximo será baixo
             return BAIXO;
-        } else if (ultimoSinal == BAIXO) { // Se o último sinal for baixo, o próximo será alto
+        } else if (temp == BAIXO) { // Se o último sinal for baixo, o próximo será alto
             return ALTO;
         } else { // Não há o que inverter
             return MEDIO;
-        }
+        }        
+    }
+    
+    // Retorna o ultimo sinal da ArrayList de sinais
+    private Integer getUltimoSinal() {
+        return sinal.get(sinal.size()-1);
     }
 }
