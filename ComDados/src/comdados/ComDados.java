91d6b65d20057f67ee58cfe8fae433ee50f1305b
João Vítor F. B <joaob20@hotmail.com>
2019-07-01 18:23:40 -0300

    ComDados modificada

--- ComDados/src/comdados/ComDados.java
+++ ComDados/src/comdados/ComDados.java
@@ -5,14 +5,51 @@
  */
 package comdados;
 
+
 /**
  *
  * @author João Vitor, Gustavo Fantinel, Bernardo
  */
 public class ComDados {
 
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private Client cliente;
+    private Server servidor;
+    
+    public ComDados () {
+        cliente = new Client();
+        servidor = new Server();
     }
+    public static void main(String[] args) {
+        ComDados programa = new ComDados();
+        
+        // Cria as threads
+        Thread threadCliente = new Thread() {
+            @Override
+            public void run() {
+                //cliente.run();
+            }
+        };
+        
+        Thread threadServidor = new Thread() {
+            @Override
+            public void run() {
+                //servidor.run();
+            }
+        };
+        
+        // Inicia as threads
+        threadServidor.start();
+        threadCliente.start();
+        
+        // Finaliza as threads
+        try {
+            threadServidor.join();
+            threadCliente.join();
+        } catch (Exception e) {
+            System.exit(0);
+        }
+        
+    }
+    
     
 }
