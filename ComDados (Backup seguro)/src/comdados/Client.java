977e501ea2e13025d6137351ba962dc2c03ca069
João Vítor F. B <37089511+joaov230@users.noreply.github.com>
2019-07-02 17:51:10 -0300

    Atualização

---
+++ ComDados (Backup seguro)/src/comdados/Client.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comdados;
+
+import java.io.DataOutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ *
+ * @author João Vitor, Gustavo Fantinel, Bernardo
+ */
+public class Client {
+    
+    private final int port;
+    
+    // Inicializador
+    public Client(int port) {
+        this.port = port;
+    }
+    
+    // Roda o cliente, que ENVIA mensagens
+    public void run() {
+        try {
+            // Inicia a conexão
+            Socket socket = new Socket ("127.0.0.1", port);
+            System.out.println("Conectado com sucesso!");
+            
+            Scanner entrada = new Scanner(System.in);
+            String str = "";
+            
+            DataOutputStream saida = new DataOutputStream(socket.getOutputStream());
+            
+            do {
+                str = entrada.nextLine();
+                
+                saida.writeUTF(str);
+                System.out.println("Mensagem enviada: " + str);
+            } while (!str.equals("Cambio"));
+
+            
+            // Depois de tudo, termina a conexão
+            entrada.close();
+            saida.close();
+            socket.close();
+            
+        } catch (Exception e) {
+            System.out.println(e);
+            System.exit(0);
+        }
+    }
+}
---
+++ ComDados (Backup seguro)/src/comdados/Server.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comdados;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author João Vitor, Gustavo Fantinel, Bernardo
+ */
+public class Server {
+    
+    private final int port;
+    
+    public Server(int port) {
+        this.port = port;
+    }
+    
+    public void run() {
+        try {
+            // Inicia o servidor seguro
+            ServerSocket servidor = new ServerSocket(port);
+            System.out.println("Servidor iniciado!");
+
+            DataInputStream entrada;
+            
+            // Tenta conectar o cliente com o servidor
+            try (Socket socket = servidor.accept()) {
+                
+                // Recebe a mensagem
+                entrada = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+                String str = "";  // Str começa vazia
+                
+                // Lê enquanto não receber "Cambio"
+                do {
+                    str = entrada.readUTF();
+                    System.out.println("Mensagem recebida: " + str);                    
+                } while (!str.equalsIgnoreCase("Cambio"));
+            }
+            
+            // Fecha todas as conexões
+            entrada.close();
+            servidor.close();
+            System.out.println("Conexão fechada!");
+            
+        } catch (Exception e) {
+            System.out.println(e);
+            System.exit(0);
+        }
+    }
+}
