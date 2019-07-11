b5d234f2eb15c241bddea7867b62e0a1378f7b09
João Vítor F. B <joaob20@hotmail.com>
2019-07-11 00:56:56 -0300

    backup seguro 2

---
+++ ComDados (Backup seguro 2)/src/comdados/ComDados.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comdados;
+
+
+/**
+ *
+ * @author João Vitor, Gustavo Fantinel, Bernardo
+ */
+public class ComDados {
+
+    private int Port = 55455;
+    private Client cliente;
+    private Server servidor;
+    
+    
+    public ComDados () {
+        cliente = new Client(Port);
+        servidor = new Server(Port);
+    }
+    
+    public void run () {
+        ///////////////////
+        // Cria as threads
+        Thread threadCliente = new Thread() {
+            @Override
+            public void run() {
+                cliente.run();
+            }
+        };
+        
+        Thread threadServidor = new Thread() {
+            @Override
+            public void run() {
+                servidor.run();
+            }
+        };
+        
+        ///////////////////
+        // Inicia as threads
+        threadServidor.start();
+        threadCliente.start();
+        
+        ///////////////////
+        // Finaliza as threads
+        try {
+            threadServidor.join();
+            threadCliente.join();
+        } catch (Exception e) {
+            System.out.println(e);
+            System.exit(0);
+        }
+    }
+    
+    public static void main(String[] args) {
+        ComDados programa = new ComDados();
+        programa.run();
+    }
+    
+    
+}
